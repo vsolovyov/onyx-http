37f310c023cd30a3e4cfe33c6612fdc6e8311fcd
Vsevolod Solovyov <vsevolod.solovyov@gmail.com>
2016-02-01 16:32:26 +0200

    0.8.8.0

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject onyx-http "0.8.6.0"
+(defproject onyx-http "0.8.8.0"
   :description "Onyx plugin for http"
   :url "https://github.com/vsolovyov/onyx-http"
+  :deploy-repositories [["releases" :clojars]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
-                 [org.onyxplatform/onyx "0.8.6"]
+                 [org.onyxplatform/onyx "0.8.8"]
                  [cc.qbits/jet "0.7.3"]]
   :profiles {:dev {:dependencies []
                    :plugins []}})
